5c810f72e598b9793bc53e87c8231c683f44ef67
Hajime Tazaki <thehajime@gmail.com>
2017-03-12 18:46:28 +0900

    matrix build netbsd/linux
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- .travis-install.sh
+++ .travis-install.sh
@@ -6,14 +6,15 @@ sudo apt-get install makefs genisoimage
 sudo apt-get install openjdk-7-jdk
 
 # Build and install rumprun toolchain from source
+RUMPKERNEL=${RUMPKERNEL:-netbsd}
 RUMPRUN_PLATFORM=${RUMPRUN_PLATFORM:-hw}
-RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE:-x86_64-rumprun-linux}
+RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE:-x86_64-rumprun-${RUMPKERNEL}}
 
 git clone -q https://github.com/libos-nuse/rumprun /tmp/rumprun
 (
 	cd /tmp/rumprun
 	git submodule update --init
-	./build-rr.sh -d /usr/local -r linux -o ./obj -qq ${RUMPRUN_PLATFORM} build
+	./build-rr.sh -d /usr/local -r ${RUMPKERNEL} -o ./obj -qq ${RUMPRUN_PLATFORM} build
 	sudo ./build-rr.sh -d /usr/local -o ./obj ${RUMPRUN_PLATFORM} install
 )
 echo RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE} >config.mk
