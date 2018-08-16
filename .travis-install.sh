1597006812104e5a3f34b86175fdf96464c82b35
Hajime Tazaki <thehajime@gmail.com>
2018-08-16 23:37:03 +0900

    test with frankenlibc+linux
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- .travis-install.sh
+++ .travis-install.sh
@@ -10,11 +10,12 @@ RUMPKERNEL=${RUMPKERNEL:-netbsd}
 RUMPRUN_PLATFORM=${RUMPRUN_PLATFORM:-hw}
 RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE:-x86_64-rumprun-${RUMPKERNEL}}
 
-git clone -q https://github.com/libos-nuse/rumprun /tmp/rumprun
-(
-	cd /tmp/rumprun
-	git submodule update --init
-	./build-rr.sh -d /usr/local -r ${RUMPKERNEL} -o ./obj -qq ${RUMPRUN_PLATFORM} build
-	sudo ./build-rr.sh -d /usr/local -o ./obj ${RUMPRUN_PLATFORM} install
-)
+#git clone -q https://github.com/libos-nuse/rumprun /tmp/rumprun
+#(
+#	cd /tmp/rumprun
+#	git submodule update --init
+#	./build-rr.sh -d /usr/local -r ${RUMPKERNEL} -o ./obj -qq ${RUMPRUN_PLATFORM} build
+#	sudo ./build-rr.sh -d /usr/local -o ./obj ${RUMPRUN_PLATFORM} install
+#)
+
 echo RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE} >config.mk
